b42ac6e45663684259d812d90f135701ac75915b
zerocold2 <ahmednasr0@hotmail.com>
2018-08-26 23:36:48 +0200

    initial

--- CrackingCodeInterview/LinkedLists.cpp
+++ CrackingCodeInterview/LinkedLists.cpp
@@ -1,7 +1,46 @@
 #include "Main.h"
 
+
+struct Node {
+
+	int Data;
+	Node* Next;
+};
+
 class LinkedLists
 {
+	Node* Head = NULL;
 public:
+	LinkedLists(int d) {
+		Head = new Node;
+		Head->Data = d;
+		Head->Next = NULL;
+	}
+	void Add(int d) {
+		Node* end = new Node;
+		end->Data = d;
+		end->Next = NULL;
+		Node* n = Head;
+		while (n->Next != NULL)n = n->Next;
+		n->Next = end;
+	}
+	void Print() {
+		while (Head->Next != NULL)
+		{
+			cout << Head->Data << " ";
+			Head = Head->Next;
+		}
+		cout << Head->Data << endl;
+	}
+	void Delete(Node* prev, Node* del) {
+		if (del->Next == NULL)
+			prev->Next = NULL;
+		else prev->Next = del->Next;
 
+	}
+
+	void RemoveDublicate(Node* Head) {
+		Node *curr = Head, *Nex = Head;
+		while (Head->)
+	}
 };
--- CrackingCodeInterview/Main.cpp
+++ CrackingCodeInterview/Main.cpp
@@ -1,6 +1,7 @@
 
 #include "PeakFinding.cpp"
 #include "ArrayAndStrings.cpp"
+#include "LinkedLists.cpp"
 
 using namespace std;
 int main()
@@ -14,7 +15,14 @@ int main()
 	int arr[10] = { 5,4,3,13,6,7,8,11,12,20 };
 
 	//cout << PeakFinding::FindPeak(arr, sizeof(arr) / sizeof(int)) << endl;
-	ArrayAndStrings::ReplaceSpaces(str);
+	//ArrayAndStrings::ReplaceSpaces(str);
+	
+	LinkedLists* list = new LinkedLists(2);
+	int data;
+	while (cin >> data && data != 0) {
+		list->Add(data);
+	}
+	list->Print();
 	system("PAUSE");
 	return 0;
 }
